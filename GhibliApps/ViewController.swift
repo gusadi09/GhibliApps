c86b2ca9bc79d6e011fdfec36c562bad55b09f5b
gusadi <gusadi872@gmail.com>
2021-07-22 19:53:41 +0800

    Initial Commit

---
+++ GhibliApps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GhibliApps
+//
+//  Created by Gus Adi on 22/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
